985bc73c49af126c45d5a79ee6ca9906638a5814
CollinKeegan <46503780+CollinKeegan@users.noreply.github.com>
2020-06-24 09:45:09 -0400

    Changed Hello World message to include name.

--- java/src/main/java/edu/ahs/robotics/java/Main.java
+++ java/src/main/java/edu/ahs/robotics/java/Main.java
@@ -2,6 +2,6 @@ package edu.ahs.robotics.java;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Android Studio!");
+        System.out.println("Hello World! My name is Collin.");
     }
 }
