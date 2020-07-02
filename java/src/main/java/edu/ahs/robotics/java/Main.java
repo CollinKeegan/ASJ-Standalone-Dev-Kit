dee01e4265eac0e36b604cfced70f08ea9cca9dd
CollinKeegan <46503780+CollinKeegan@users.noreply.github.com>
2020-07-02 17:26:26 -0400

    Changed Hello World message to include name.

--- java/src/main/java/edu/ahs/robotics/java/Main.java
+++ java/src/main/java/edu/ahs/robotics/java/Main.java
@@ -1,7 +1,14 @@
 package edu.ahs.robotics.java;
 
 public class Main {
+
+    /*public double getX (char x){
+        return(x.GetX());
+    }*/
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World! This is a test. shsfghgfshdfgfh");
+        Point a = new Point(100, 100);
+        System.out.println(a.getCoords().X());
     }
 }
